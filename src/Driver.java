e5a9673a93498a44a07081b722de612bf5b20094
akinkayagithub <akinkaya.b@gmail.com>
2021-11-25 11:49:19 -0600

    added singleton

---
+++ src/Driver.java
@@ -0,0 +1,20 @@
+public class Driver {
+
+    //private instance of Driver cannot be accessed anywhere except this class
+    private static Driver driver;
+
+    //private constructor will prevent Driver class to be instantiated
+    private Driver(){
+
+    }
+
+    //public getter method is the single access point to get Driver object, and it also ensures a second object is not getting instantiated
+    public static Driver getDriver(){
+        if(driver == null){
+            //A new object will be instantiated only if the driver is null
+            //This block of code can be used to set driver in different shapes using switch case statements
+            driver = new Driver();
+        }
+        return driver;
+    }
+}
